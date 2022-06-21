1223e92550088e4e6a1ac20614d884cc7ffd2cf4
Pranav Kumar <pranavismic@gmail.com>
2022-06-21 20:13:36 +0530

    fixed sizing issue in mobile view

--- components/ExerciseCard.js
+++ components/ExerciseCard.js
@@ -6,9 +6,11 @@ const ExerciseCard = ({gifUrl, name, id}) => {
         <Box
         boxShadow="lg" 
         p='0.5rem'
+        w={['75%', '75%', '100%', '100%']}
         h='320px'
         rounded="md" 
         bg="white"
+        m='auto'
         display='flex'
         flexDirection='column'
         textAlign='center'
